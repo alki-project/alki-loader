ffaf01a3e8ad461eabf868357319a4a7dad6e0fa
Matt Edlefsen <matt.edlefsen@gmail.com>
2017-03-04 23:01:56 -0800

    Make registry thread safe

--- lib/alki/loader/registry.rb
+++ lib/alki/loader/registry.rb
@@ -1,16 +1,19 @@
+require 'thread'
+
 module Alki
   module Loader
     class Registry
       def initialize
+        @lock = Monitor.new
         clear
       end
 
       def clear
-        @paths = []
-        @paths_sorted = true
-        @names = []
-        @names_sorted = true
-        @configs_loaded = false
+        @lock.synchronize do
+          @paths = []
+          @names = []
+          @configs_loaded = false
+        end
       end
 
       def paths
@@ -19,20 +22,20 @@ module Alki
       end
 
       def add(entry)
-        @paths.delete_if{|e| e.path == entry.path }
-        @paths << entry
-        if entry.name
-          @names.delete_if{|e| e.path == entry.path }
-          @names << entry
+        @lock.synchronize do
+          @paths.delete_if{|e| e.path == entry.path }
+          @paths << entry
+          @names.sort!{|a,b| b.path <=> a.path}
+          if entry.name
+            @names.delete_if{|e| e.path == entry.path }
+            @names << entry
+            @names.sort!{|a,b| b.name <=> a.name}
+          end
         end
       end
 
       def lookup_name(name)
         load_configs!
-        unless @names_sorted
-          @names.sort! {|a,b| b.name <=> a.name }
-          @names_sorted = true
-        end
         @names.find do |e|
           name.start_with?(e.name) &&
             (name.size == e.name.size || name[e.name.size] == '/')
@@ -41,10 +44,6 @@ module Alki
 
       def lookup_path(path)
         load_configs!
-        unless @paths_sorted
-          @paths.sort! {|a,b| b.path <=> a.path }
-          @paths_sorted = true
-        end
         @paths.find do |e|
           path.start_with?(e.path) &&
             (path.size == e.path.size || path[e.path.size] == '/')
@@ -52,11 +51,13 @@ module Alki
       end
 
       def load_configs!
-        path_hash = $LOAD_PATH.hash
-        return if @configs_loaded == path_hash
-        @configs_loaded = path_hash
-        Gem.find_files_from_load_path('alki_loader.rb').each do |config_path|
-           require config_path
+        @lock.synchronize do
+          path_hash = $LOAD_PATH.hash
+          return if @configs_loaded == path_hash
+          @configs_loaded = path_hash
+          Gem.find_files_from_load_path('alki_loader.rb').each do |config_path|
+            require config_path
+          end
         end
       end
     end
